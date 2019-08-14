911f5a1161d941ba5eb2e60f6e3f718dcb04d27c
gegorov <gegorov@yahoo.com>
2019-08-15 00:22:24 +0300

    add redux store and actions

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route } from 'react-router';
+import { Provider } from 'react-redux';
+
+import store, { history } from './store';
 
 import './styles/style.styl';
 
-import Main from './components/Main';
+
 import Single from './components/Single';
 import PhotoGrid from './components/PhotoGrid';
 
 const router = (
-  <Router history={browserHistory}>
-    
-    <Route path="/" component={PhotoGrid}>
-    </Route>
-    <Route path="/view/:postId"  component={Single}>
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={PhotoGrid}>
+      </Route>
+      <Route path="/view/:postId"  component={Single}>
+      </Route>
+    </Router>
+  </Provider>
 );
 
 
